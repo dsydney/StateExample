2fae44ba4cddb35bb8b7aec7dafb58f6e0e5a1df
David Sydney <dsydney42@gmail.com>
2022-04-11 09:58:26 -0500

    Final commit

---
+++ app/src/main/java/com/example/stateexample/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.stateexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.stateexample.ui.theme.StateExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StateExampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    DemoScreen()
+                    //FunctionA()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DemoScreen() {
+    var textState by rememberSaveable { mutableStateOf("") }
+
+    val onTextChange = { text: String ->
+        textState = text
+    }
+    MyTextField(text = textState, onTextChange = onTextChange)
+}
+
+@Composable
+fun MyTextField(text: String, onTextChange: (String) -> Unit) {
+    TextField(value = text, onValueChange = onTextChange)
+}
+
+@Composable
+fun FunctionA() {
+    var switchState by remember { mutableStateOf(true) }
+
+    val onSwitchChange = { value: Boolean ->
+        switchState = value
+    }
+
+    FunctionB(
+        switchState = switchState,
+        onSwitchChange = onSwitchChange
+    )
+}
+
+@Composable
+fun FunctionB(switchState: Boolean, onSwitchChange: (Boolean) -> Unit) {
+    Switch(
+        checked = switchState,
+        onCheckedChange = onSwitchChange
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    StateExampleTheme {
+        DemoScreen()
+    }
+}
